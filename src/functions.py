9297ffb47db5c2c99d2201b337a0ca9980a64053
x22a <xabiergdvz@gmail.com>
2020-01-31 19:26:19 +0100

    Jupyter optimized

--- src/functions.py
+++ src/functions.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 
 def dateTo(dataframe):
-
+    dataframe = dataframe.drop(["Area", "Fossil Coal-derived gas  - Actual Aggregated [MW]", "Fossil Oil shale  - Actual Aggregated [MW]", "Fossil Peat  - Actual Aggregated [MW]", "Geothermal  - Actual Aggregated [MW]", "Hydro Pumped Storage  - Actual Aggregated [MW]", "Marine  - Actual Aggregated [MW]", "Wind Offshore  - Actual Aggregated [MW]"], axis=1)
     Hour = pd.Series([])
     Day = pd.Series([])
     Month = pd.Series([])
@@ -26,5 +26,25 @@ def dateTo(dataframe):
     dataframe.insert(0, "Hour", Hour) 
 
     #Pendiente quitar la columna MTU
+    dataframe = dataframe.drop(["MTU"], axis=1)
+
+    newNames = {
+    'Biomass  - Actual Aggregated [MW]': 'Biomass MWh',
+    'Fossil Brown coal/Lignite  - Actual Aggregated [MW]': 'Fossil Lignite MWh',
+    'Fossil Gas  - Actual Aggregated [MW]': "Fossil Gas MWh",
+    'Fossil Hard coal  - Actual Aggregated [MW]': "Fossil Coal MWh",
+    'Fossil Oil  - Actual Aggregated [MW]': "Fossil Oil MWh",
+    'Hydro Pumped Storage  - Actual Consumption [MW]': "Hydro Pumped Storage MWh",
+    'Hydro Run-of-river and poundage  - Actual Aggregated [MW]': "Hydro Run-of-river MWh",
+    'Hydro Water Reservoir  - Actual Aggregated [MW]': "Hydro Water Reservoir MWh",
+    'Nuclear  - Actual Aggregated [MW]': "Nuclear MWh",
+    'Other  - Actual Aggregated [MW]': "Other MWh",
+    'Other renewable  - Actual Aggregated [MW]': "Other Renewable MWh",
+    'Solar  - Actual Aggregated [MW]': "Solar MWh",
+    'Waste  - Actual Aggregated [MW]': "Waste MWh",
+    'Wind Onshore  - Actual Aggregated [MW]': "Wind MWh"
+    }
+
+    dataframe = dataframe.rename(columns=newNames, errors="raise")
 
     return dataframe
