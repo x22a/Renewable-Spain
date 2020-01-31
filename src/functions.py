3c555ece0eab9e0876cd3def1d5f88ffd6d9c8f5
x22a <xabiergdvz@gmail.com>
2020-01-31 19:07:30 +0100

    .gitignore modified and function corrected in functions

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,13 @@
-def dateTo(dataframe)
+import re
+import pandas as pd
+
+def dateTo(dataframe):
+
+    Hour = pd.Series([])
+    Day = pd.Series([])
+    Month = pd.Series([])
+    Year = pd.Series([])
+
     index = 0
     for date in dataframe.MTU:
         matchHour = re.findall(r"[0-9]{2}:[0-9]{2}\s", date)
@@ -9,11 +18,13 @@ def dateTo(dataframe)
         Month[index] = matchMonth[0]
         yearMatch = re.findall(r"(?<=\.)([0-9]{4})", date)
         Year[index] = yearMatch[0]
-        index += 1(self, parameter_list):
+        index += 1
     
     dataframe.insert(0, "Year", Year) 
     dataframe.insert(0, "Day", Day) 
     dataframe.insert(0, "Month", Month) 
     dataframe.insert(0, "Hour", Hour) 
+
+    #Pendiente quitar la columna MTU
 
     return dataframe
